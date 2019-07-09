fee3c07a6279fea8f55a4482d4d3c6c55a43cd34
Artem <shliapin.ae@phystech.edu>
2019-07-09 14:53:43 +0300

    Artem commit
    New app

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def show_main_page():
     return "Hello world!"
 
+@app.route('/Artem')
+def show_main_page():
+    return "Zdravstvuyte"
+
 
 if __name__ == "__main__":
     app.run()
